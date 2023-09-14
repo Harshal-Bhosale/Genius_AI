c24b407e445c7910c45e8dd2321e3714c424c629
Harshal Bhosale <bharshal000@gmail.com>
2023-09-15 02:07:43 +0530

    INR-USD

--- app/api/stripe/route.ts
+++ app/api/stripe/route.ts
@@ -41,12 +41,12 @@ export async function GET() {
       line_items: [
         {
           price_data: {
-            currency: "USD",
+            currency: "INR",
             product_data: {
               name: "Genius Pro",
               description: "Unlimited AI Generations"
             },
-            unit_amount: 2000,
+            unit_amount: 14900,
             recurring: {
               interval: "month"
             }
